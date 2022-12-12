78f04b53690aa02e76e69123b70c860534017fe1
Kei <budi03@gmail.com>
2022-12-13 04:38:29 +0700

    U-221213043829

---
+++ app/Actions/QrCode/QrCodeAction.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\QrCode;
+
+use Endroid\QrCode\Builder\Builder;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
+use Shlinkio\Shlink\Common\Response\QrCodeResponse;
+use Shlinkio\Shlink\Core\Action\Model\QrCodeParams;
+use Shlinkio\Shlink\Core\Exception\ShortUrlNotFoundException;
+use Shlinkio\Shlink\Core\Options\QrCodeOptions;
+use Shlinkio\Shlink\Core\ShortUrl\Helper\ShortUrlStringifierInterface;
+use Shlinkio\Shlink\Core\ShortUrl\Model\ShortUrlIdentifier;
+use Shlinkio\Shlink\Core\ShortUrl\ShortUrlResolverInterface;
+
+class QrCodeAction implements MiddlewareInterface
+{
+    public function __construct(
+        private ShortUrlResolverInterface $urlResolver,
+        private ShortUrlStringifierInterface $stringifier,
+        private LoggerInterface $logger,
+        private QrCodeOptions $defaultOptions,
+    ) {
+    }
+
+    public function process(Request $request, RequestHandlerInterface $handler): Response
+    {
+        $identifier = ShortUrlIdentifier::fromRedirectRequest($request);
+
+        try {
+            $shortUrl = $this->urlResolver->resolveEnabledShortUrl($identifier);
+        } catch (ShortUrlNotFoundException $e) {
+            $this->logger->warning('An error occurred while creating QR code. {e}', ['e' => $e]);
+            return $handler->handle($request);
+        }
+
+        $params = QrCodeParams::fromRequest($request, $this->defaultOptions);
+        $qrCodeBuilder = Builder::create()
+            ->data($this->stringifier->stringify($shortUrl))
+            ->size($params->size)
+            ->margin($params->margin)
+            ->writer($params->writer)
+            ->errorCorrectionLevel($params->errorCorrectionLevel)
+            ->roundBlockSizeMode($params->roundBlockSizeMode);
+
+        return new QrCodeResponse($qrCodeBuilder->build());
+    }
+}
---
+++ app/Actions/QrCode/QrCodeParams.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\QrCode;
+
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelInterface;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelLow;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelMedium;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelQuartile;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeInterface;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeNone;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Writer\SvgWriter;
+use Endroid\QrCode\Writer\WriterInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Shlinkio\Shlink\Core\Options\QrCodeOptions;
+
+use function Functional\contains;
+use function strtolower;
+use function trim;
+
+final class QrCodeParams
+{
+    private const MIN_SIZE = 50;
+    private const MAX_SIZE = 1000;
+    private const SUPPORTED_FORMATS = ['png', 'svg'];
+
+    private function __construct(
+        public readonly int $size,
+        public readonly int $margin,
+        public readonly WriterInterface $writer,
+        public readonly ErrorCorrectionLevelInterface $errorCorrectionLevel,
+        public readonly RoundBlockSizeModeInterface $roundBlockSizeMode,
+    ) {
+    }
+
+    public static function fromRequest(ServerRequestInterface $request, QrCodeOptions $defaults): self
+    {
+        $query = $request->getQueryParams();
+
+        return new self(
+            self::resolveSize($query, $defaults),
+            self::resolveMargin($query, $defaults),
+            self::resolveWriter($query, $defaults),
+            self::resolveErrorCorrection($query, $defaults),
+            self::resolveRoundBlockSize($query, $defaults),
+        );
+    }
+
+    private static function resolveSize(array $query, QrCodeOptions $defaults): int
+    {
+        $size = (int) ($query['size'] ?? $defaults->size);
+        if ($size < self::MIN_SIZE) {
+            return self::MIN_SIZE;
+        }
+
+        return $size > self::MAX_SIZE ? self::MAX_SIZE : $size;
+    }
+
+    private static function resolveMargin(array $query, QrCodeOptions $defaults): int
+    {
+        $margin = $query['margin'] ?? (string) $defaults->margin;
+        $intMargin = (int) $margin;
+        if ($margin !== (string) $intMargin) {
+            return 0;
+        }
+
+        return $intMargin < 0 ? 0 : $intMargin;
+    }
+
+    private static function resolveWriter(array $query, QrCodeOptions $defaults): WriterInterface
+    {
+        $qFormat = self::normalizeParam($query['format'] ?? '');
+        $format = contains(self::SUPPORTED_FORMATS, $qFormat) ? $qFormat : self::normalizeParam($defaults->format);
+
+        return match ($format) {
+            'svg' => new SvgWriter(),
+            default => new PngWriter(),
+        };
+    }
+
+    private static function resolveErrorCorrection(array $query, QrCodeOptions $defaults): ErrorCorrectionLevelInterface
+    {
+        $errorCorrectionLevel = self::normalizeParam($query['errorCorrection'] ?? $defaults->errorCorrection);
+        return match ($errorCorrectionLevel) {
+            'h' => new ErrorCorrectionLevelHigh(),
+            'q' => new ErrorCorrectionLevelQuartile(),
+            'm' => new ErrorCorrectionLevelMedium(),
+            default => new ErrorCorrectionLevelLow(), // 'l'
+        };
+    }
+
+    private static function resolveRoundBlockSize(array $query, QrCodeOptions $defaults): RoundBlockSizeModeInterface
+    {
+        $doNotRoundBlockSize = isset($query['roundBlockSize'])
+            ? $query['roundBlockSize'] === 'false'
+            : ! $defaults->roundBlockSize;
+        return $doNotRoundBlockSize ? new RoundBlockSizeModeNone() : new RoundBlockSizeModeMargin();
+    }
+
+    private static function normalizeParam(string $param): string
+    {
+        return strtolower(trim($param));
+    }
+}
