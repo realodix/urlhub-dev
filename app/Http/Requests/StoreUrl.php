67ce0c17b40a53822ebb043d6f4d9b52dd31e597
Kei <budi03@gmail.com>
2022-12-16 02:52:34 +0700

    Update StoreUrl.php

--- app/Http/Requests/StoreUrl.php
+++ app/Http/Requests/StoreUrl.php
@@ -26,7 +26,7 @@ class StoreUrl extends FormRequest
     public function rules()
     {
         return [
-            'long_url'   => ['required', 'url', 'max:65535', new DomainBlacklist],
+            'destination'   => ['required', 'url', 'max:65535', new DomainBlacklist],
             'custom_key' => ['nullable', 'max:20', new StrAlphaUnderscore, 'unique:urls,keyword'],
         ];
     }
@@ -39,7 +39,7 @@ class StoreUrl extends FormRequest
     public function messages()
     {
         return [
-            'long_url.required' => __('The URL field must be filled, should not be empty.'),
+            'destination.required' => __('The URL field must be filled, should not be empty.'),
             'custom_key.max'    => __('The custom url may not be greater than :max characters.'),
             'custom_key.unique' => __(':input has already been taken'),
         ];
