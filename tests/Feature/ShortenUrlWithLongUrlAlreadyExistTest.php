6f1551d9b11480943e2cb2339c296f36c2a2375d
Kei <budi03@gmail.com>
2022-12-16 13:19:15 +0700

    U-221216131915

--- tests/Feature/ShortenUrlWithLongUrlAlreadyExistTest.php
+++ tests/Feature/ShortenUrlWithLongUrlAlreadyExistTest.php
@@ -216,8 +216,8 @@ class ShortenUrlWithLongUrlAlreadyExistTest extends TestCase
         $url = Url::factory()->create();
 
         $response = $this->post(route('su_create'), [
-            'long_url' => 'https://laravel-news.com',
-            'custom_key'  => $url->keyword,
+            'long_url'   => 'https://laravel-news.com',
+            'custom_key' => $url->keyword,
         ]);
 
         $response
@@ -238,8 +238,8 @@ class ShortenUrlWithLongUrlAlreadyExistTest extends TestCase
 
         $response = $this->actingAs($this->nonAdmin())
             ->post(route('su_create'), [
-                'long_url' => 'https://laravel-news.com',
-                'custom_key'  => $url->keyword,
+                'long_url'   => 'https://laravel-news.com',
+                'custom_key' => $url->keyword,
             ]);
 
         $response
