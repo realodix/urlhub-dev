60782a389b63b611acff403453bbfa45d3d0f90d
Kei <budi03@gmail.com>
2023-01-12 06:19:35 +0700

    Feature\AuthPage

--- tests/Feature/AuthPage/DashboardPageTest.php
+++ tests/Feature/AuthPage/DashboardPageTest.php
@@ -1,28 +1,19 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\AuthPage;
 
 use App\Models\Url;
 use Tests\TestCase;
 use Vinkla\Hashids\Facades\Hashids;
 
-/**
- * Back-End Test.
- */
-class UrlBeTest extends TestCase
+class DashboardPageTest extends TestCase
 {
     protected function hashIdRoute($routeName, $url_id)
     {
-        $hashids = Hashids::connection(\App\Models\Url::class);
+        $hashids = Hashids::connection(Url::class);
 
         return route($routeName, $hashids->encode($url_id));
     }
-
-    /*
-    |--------------------------------------------------------------------------
-    | Dashboard Page
-    |--------------------------------------------------------------------------
-    */
 
     /**
      * @test
@@ -119,99 +110,5 @@ class UrlBeTest extends TestCase
             ->assertSessionHas('flash_success');
 
         $this->assertSame($newLongUrl, $url->fresh()->destination);
-    }
-
-    /*
-    |--------------------------------------------------------------------------
-    | All URLs Page
-    |--------------------------------------------------------------------------
-    */
-
-    /**
-     * @test
-     * @group f-allurl
-     */
-    public function auAdminCanAccessThisPage()
-    {
-        $response = $this->actingAs($this->admin())
-            ->get(route('dashboard.allurl'));
-
-        $response->assertOk();
-    }
-
-    /**
-     * @test
-     * @group f-allurl
-     */
-    public function auNonAdminCantAccessThisPage()
-    {
-        $response = $this->actingAs($this->nonAdmin())
-            ->get(route('dashboard.allurl'));
-
-        $response->assertForbidden();
-    }
-
-    /**
-     * @test
-     * @group f-allurl
-     */
-    public function auAdminCanDelete()
-    {
-        $url = Url::factory()->create();
-
-        $response = $this->actingAs($this->admin())
-            ->from(route('dashboard.allurl'))
-            ->get($this->hashIdRoute('dashboard.allurl.su_delete', $url->id));
-
-        $response->assertRedirectToRoute('dashboard.allurl')
-            ->assertSessionHas('flash_success');
-
-        $this->assertCount(0, Url::all());
-    }
-
-    /**
-     * @test
-     * @group f-allurl
-     */
-    public function auNonAdminCantDelete()
-    {
-        $url = Url::factory()->create();
-
-        $response = $this->actingAs($this->nonAdmin())
-            ->from(route('dashboard.allurl'))
-            ->get($this->hashIdRoute('dashboard.allurl.su_delete', $url->id));
-
-        $response->assertForbidden();
-        $this->assertCount(1, Url::all());
-    }
-
-    /*
-    |--------------------------------------------------------------------------
-    | All Users Page
-    |--------------------------------------------------------------------------
-    */
-
-    /**
-     * @test
-     * @group f-alluser
-     */
-    public function ausAdminCanAccessThisPage()
-    {
-        $response = $this->actingAs($this->admin())
-            ->get(route('user.index'));
-
-        $response->assertOk();
-    }
-
-    /**
-     * @test
-     * @group f-alluser
-     */
-    public function ausNonAdminCantAccessThisPage()
-    {
-        $response = $this->actingAs($this->nonAdmin())
-            ->get(route('user.index'));
-
-        $response->assertForbidden();
     }
 }
