1872546c62337476e0b519f17cb4749234518985
Kei <budi03@gmail.com>
2023-01-12 06:29:30 +0700

    Feature\CustomKeyValidationTest.php

---
+++ tests/Feature/CustomKeyValidationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class CustomKeyValidationTest extends TestCase
+{
+    /** @test */
+    public function customKeyValidation()
+    {
+        $component = \Livewire\Livewire::test(\App\Http\Livewire\UrlCheck::class);
+
+        $component->assertStatus(200)
+            ->set('keyword', '!')
+            ->assertHasErrors('keyword')
+            ->set('keyword', 'FOO')
+            ->assertHasErrors('keyword')
+            ->set('keyword', 'admin')
+            ->assertHasErrors('keyword')
+            ->set('keyword', 'foo_bar')
+            ->assertHasNoErrors('keyword');
+    }
+}
