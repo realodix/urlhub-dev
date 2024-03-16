8622cc294ccc65f4e021173951270760283f34a9
Kei <budi03@gmail.com>
2024-03-17 03:51:42 +0700

    Slim skeleton

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\HelperServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
 ];
